313341dbff5f96bfbf7c14d91b7016eb9bdf7066
allcookiesaccept <insufferable.lobster@gmail.com>
2023-07-01 13:48:15 +0300

    Features: DescriptionsFiller class: CSWorker

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ async def main():
 
     dp.include_router(user.router)
 
-    # await bot.delete_webhook(drop_pending_updates=True)
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 
