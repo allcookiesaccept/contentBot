f9e042bf9bbebc2af3916bfe182c9db2808af50d
allcookiesaccept <insufferable.lobster@gmail.com>
2023-08-08 14:33:43 +0300

    stable version 0.1.1

--- services/xml/parser.py
+++ services/xml/parser.py
@@ -3,13 +3,13 @@ import httpx
 import xml.etree.ElementTree as ET
 from db.data import FEEDS, COLUMNS
 
-class XMLParser:
 
+class XMLParser:
     FEEDS = FEEDS
     COLUMNS = COLUMNS
+
     def __init__(self):
         self.parser_type = None
-        self.acceptor = []
         self.site_acceptor = None
 
     def get_offers_from_xml(self, url: str) -> list:
@@ -39,28 +39,43 @@ class XMLParser:
             return pd.DataFrame(columns=["id", "external"])
 
     def create_empty_products_df(self) -> pd.DataFrame:
-
         xml_offers = None
 
         if type(self.parser_type) == str and type(self.site_acceptor) == str:
             try:
-                empty_product_slugs = self.parser_type.split('_')
-                empty_product_string = f"{empty_product_slugs[0]}out_{empty_product_slugs[1]}"
-                empty_product_feed = self.FEEDS[self.site_acceptor][empty_product_string]
+                empty_product_slugs = self.parser_type.split("_")
+                empty_product_string = (
+                    f"{empty_product_slugs[0]}out_{empty_product_slugs[1]}"
+                )
+                empty_product_feed = self.FEEDS[self.site_acceptor][
+                    empty_product_string
+                ]
                 xml_offers = self.get_offers_from_xml(empty_product_feed)
             except Exception as ex:
                 print(ex)
+        else:
+            raise Exception(
+                f"Parser type:{str(self.parser_type)}\tSitename:{str(self.site_acceptor)}"
+            )
 
         empty_products = self.extract_empty_products_data(xml_offers)
+
         return empty_products
 
     def collect_donor_feeds(self) -> list:
-
         if type(self.parser_type) == str and type(self.site_acceptor) == str:
             try:
-                self.donor_feeds = list(x[self.parser_type] for x in self.FEEDS.values())
-                self.donor_feeds.remove(self.FEEDS[self.site_acceptor][self.parser_type])
+                self.donor_feeds = list(
+                    x[self.parser_type] for x in self.FEEDS.values()
+                )
+                self.donor_feeds.remove(
+                    self.FEEDS[self.site_acceptor][self.parser_type]
+                )
             except Exception as ex:
                 raise ()
+        else:
+            raise Exception(
+                f"Parser type:{str(self.parser_type)}\tSitename:{str(self.site_acceptor)}"
+            )
 
         return self.donor_feeds
